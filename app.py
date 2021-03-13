23cd89d76e3853b2a4951e7686f01ef1a4c6a125
AndrewBall79 <andy.bob.ball@gmail.com>
2021-03-13 14:52:56 -0600

    python

--- app.py
+++ app.py
@@ -1,28 +1,46 @@
-character_name = "Hugo"
-character_age = "35"
-is_male = True
+from math import *
 
-print("There once was a man named "+character_name+", ")
-print("he was " + character_age + " years old. ")
+# character_name = "Hugo"
+# character_age = 35
+# is_male = True
+#
+# print("There once was a man named "+character_name+", ")
+# print("he was " + str(character_age) + " years old. ")
+#
+# character_name = "Doogie"
+#
+# print("He really liked the name "+character_name+", ")
+# print("but he didn't like being "+f'{character_age}'+".")
+# print("")
+#
+# phrase = "Giraffe Academy"
+# print("Strings are \"Fun\"")
+# print("")
+# print(phrase + "is NOT cool, hahaha.")
+# print("is" + phrase.lower() + " " + phrase.upper() + " cool?")
+# print(phrase.isupper())
+# print("")
+# print("is" + phrase.lower() + " " + phrase.upper() + " cool?")
+# print(phrase.upper().isupper())
+# print("")
+# phrase_length = len(phrase)
+# print("The phrase is " + str(phrase_length) + " characters long")
+# print("")
+# print(phrase.replace("Giraffe", "Fat Cats"))
+# print("")
+# my_num = -2
+# print("The absolute value of my_num is: " + str(abs(my_num)))
+# print()
+# print(pow(4, 6))
+# print()
+# print(max(484, 829))
+# print(max(484, 829))
+# print(sqrt(36))
+# math. floor, round, max, min, sqrt, ceil,
 
-character_name = "Doogie"
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello " + name + "!")
+print("Your are " + age)
 
-print("He really liked the name "+character_name+", ")
-print("but he didn't like being "+character_age+".")
-print("")
-
-phrase = "Giraffe Academy"
-print("Strings are \"Fun\"")
-print("")
-print(phrase + "is NOT cool, hahaha.")
-print("is" + phrase.lower() + " " + phrase.upper() + " cool?")
-print(phrase.isupper())
-print("")
-print("is" + phrase.lower() + " " + phrase.upper() + " cool?")
-print(phrase.upper().isupper())
-print("")
-phrase_length = len(phrase)
-print("The phrase is " + str(phrase_length) + " characters long")
-print("")
-print(phrase.replace("Giraffe", "Fat Cats"))
 
