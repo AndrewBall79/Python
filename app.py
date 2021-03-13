84b333413a965c925438b209c04024751c1ecc91
AndrewBall79 <andy.bob.ball@gmail.com>
2021-03-13 14:30:27 -0600

    python

---
+++ app.py
@@ -0,0 +1,28 @@
+character_name = "Hugo"
+character_age = "35"
+is_male = True
+
+print("There once was a man named "+character_name+", ")
+print("he was " + character_age + " years old. ")
+
+character_name = "Doogie"
+
+print("He really liked the name "+character_name+", ")
+print("but he didn't like being "+character_age+".")
+print("")
+
+phrase = "Giraffe Academy"
+print("Strings are \"Fun\"")
+print("")
+print(phrase + "is NOT cool, hahaha.")
+print("is" + phrase.lower() + " " + phrase.upper() + " cool?")
+print(phrase.isupper())
+print("")
+print("is" + phrase.lower() + " " + phrase.upper() + " cool?")
+print(phrase.upper().isupper())
+print("")
+phrase_length = len(phrase)
+print("The phrase is " + str(phrase_length) + " characters long")
+print("")
+print(phrase.replace("Giraffe", "Fat Cats"))
+
