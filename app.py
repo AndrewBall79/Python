ccd46640418e768bbd13a20445248a765a285957
AndrewBall79 <andy.bob.ball@gmail.com>
2021-03-15 14:05:31 -0500

    readfile

--- app.py
+++ app.py
@@ -260,3 +260,16 @@ from math import *
 # except ZeroDivisionError as err:
 #     print(err)
 
+
+employee_file = open("employees.txt", "r")
+for employee in employee_file.readlines():
+#     print(employee)
+# print(employee_file.readable())
+# print("")
+# print(employee_file.readline())
+# print(employee_file.readline())
+# print("")
+# print(employee_file.readlines()[1])
+
+employee_file.close()
+
