e33ebccf4b4864b88af56610ab7b6d7282bb1932
AndrewBall79 <andy.bob.ball@gmail.com>
2021-03-24 16:14:05 -0500

    pass

--- age.py
+++ age.py
@@ -1,5 +1,7 @@
 from datetime import date
 # request birth year
+from getpass import getpass
+
 birth_year = int(input('what year were you born? '))
 # create the date object
 today_date = date.today()
@@ -9,3 +11,12 @@ cur_year = today_date.year
 age = cur_year - birth_year
 # Print the users age
 print(f"You are {age} years old")
+
+
+user_name = input("What is your username? ")
+password = getpass("What is your password? ")
+password_length = len(password) * '*'
+hidden_password = len(password)
+
+print(f"{user_name}, your password {password_length}, is {hidden_password} characters long")
+
