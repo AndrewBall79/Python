5e6ce234d3c4fa1b7d539017e421fd684144866d
AndrewBall79 <andy.bob.ball@gmail.com>
2021-03-28 14:00:28 -0500

    iterables

--- age.py
+++ age.py
@@ -20,3 +20,21 @@ hidden_password = len(password)
 
 print(f"{user_name}, your password {password_length}, is {hidden_password} characters long")
 
+
+user = {
+    'name': "Golem",
+    'age': 5006,
+    'can_swim': False,
+}
+
+for item in user.items():
+    print(item)
+
+for key, value in user.items():
+    print(key, value)
+
+for item in user.values():
+    print(item)
+
+for item in user.keys():
+    print(item)
